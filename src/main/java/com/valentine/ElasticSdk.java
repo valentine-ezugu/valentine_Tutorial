64e329d9fb59f5d02ce2d84f68b7475f79bd6047
valentine-ezugu <valentineezugu@yahoo.com>
2018-07-25 20:14:51 +0300

    added delete bulk method

--- src/main/java/com/valentine/ElasticSdk.java
+++ src/main/java/com/valentine/ElasticSdk.java
@@ -24,30 +24,25 @@ public class ElasticSdk {
 
 
     Settings settings = Settings.builder().put("cluster.name", "elasticSearch").build();
-    TransportClient client = new PreBuiltTransportClient(settings).addTransportAddress(new TransportAddress(InetAddress.getByName("host1"), 9300)).addTransportAddress(new TransportAddress(InetAddress.getByName("host2"), 9300));
+    TransportClient client = new PreBuiltTransportClient(settings).
+        addTransportAddress(new TransportAddress(InetAddress.getByName("host1"), 9300)).
+        addTransportAddress(new TransportAddress(InetAddress.getByName("host2"), 9300));
     //client.close();
 
 
-    XContentBuilder builder = jsonBuilder().startObject().field("user", "kimchy").field("postDate", new Date()).field("message", "trying out Elasticsearch").endObject();
+    XContentBuilder builder = jsonBuilder().
+        startObject().field("user", "kimchy").
+        field("postDate", new Date()).field("message", "trying out Elasticsearch")
+        .endObject();
 
-     // if u need to see the generated json
+     // if u need to see the generated json for test purposes may be
     // String json = Strings.toString(builder);
 
 
-    IndexResponse response = client.prepareIndex("twitter", "tweet", "1").setSource(jsonBuilder().startObject().array("user", "kimchy", "valik") //array testing
-        .field("postDate", new Date()).field("message", "trying out Elasticsearch").endObject()).get();
-
-
-//    String string = "{" +
-//        "\"user\":\"kimchy\"," +
-//        "\"postDate\":\"2013-01-30\"," +
-//        "\"message\":\"trying out Elasticsearch\"" +
-//        "}";
-//
-//    IndexResponse indexResponse = client.prepareIndex("twitter", "tweet")
-//        .setSource(string, XContentType.JSON)
-//        .get();
-
+    IndexResponse response = client.prepareIndex("twitter", "tweet", "1").
+        setSource(jsonBuilder().startObject().array("user", "kimchy", "valik") //array testing
+        .field("postDate", new Date()).field("message", "trying out Elasticsearch").
+                endObject()).get();
 
     // Index name
     String _index = response.getIndex();
@@ -63,24 +58,34 @@ public class ElasticSdk {
     GetResponse getResponse = client.prepareGet("twitter", "tweet", "1").get();
 
 
+    /**
+     * This method deletes all persons with job = accounting, it first searches all specified indices
+     * , gets all persons that match query then delete
+     *
+     * this deletes in bulk.
+     */
+    public void deleteAllAccountants() {
+        BulkByScrollResponse    response = DeleteByQueryAction.INSTANCE.newRequestBuilder(client)
+            .filter(QueryBuilders.matchQuery("job", "accounting")).
+            //here we can list as many indices as posible to search from
+                source("Person", "","").
+                get("time up");
+        long deleted = response.getDeleted();
 
+    }
 
-        //for deleting bulk
-    BulkByScrollResponse response1 = DeleteByQueryAction.INSTANCE.newRequestBuilder(client)
-        .filter(QueryBuilders.matchQuery("gender", "male"))
-        .source("persons")
-        .get();
-    long deleted = response1.getDeleted();
-
-
-
-
+    /**
+     *
+     * @return
+     */
     public String deleteInBulkAsync() {
+
         // do async
         DeleteByQueryAction.INSTANCE.newRequestBuilder(client)
             .filter(QueryBuilders.matchQuery("gender", "male"))
             .source("persons")
             .execute(new ActionListener<BulkByScrollResponse>() {
+
                 @Override
                 public void onResponse(BulkByScrollResponse response) {
                     long deleted = response.getDeleted();
